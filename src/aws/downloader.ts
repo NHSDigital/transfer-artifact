ea369fcb8d576899520fea2f350463dd326949b8
Alice Bell <alice.bell@bjss.com>
2024-02-13 16:00:39 +0000

    CAAS-2009: added writes3ObjectToFile

--- src/aws/downloader.ts
+++ src/aws/downloader.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 import {getInputs} from '../input-helper';
 import {listS3Objects} from "../aws";
+import { writeS3ObjectToFile } from './get-object-s3';
 
 export async function runDownload(): Promise<void> {
     try {
@@ -8,10 +9,18 @@ export async function runDownload(): Promise<void> {
         const inputs = getInputs();
         const myBucket = inputs.artifactBucket;
         const myName = inputs.artifactName;
+        const myLocation = { Bucket: myBucket, Key: myName }
+        console.log('I am doing listS3Objects:')
         listS3Objects({
           Bucket: myBucket,
           Key: myName
         })
+        console.log('I am doing writeS3ObjectToFile:')
+        await writeS3ObjectToFile(
+          myLocation,
+          myName
+        )
+
     } catch (error) {
         core.setFailed((error as Error).message)
       }
