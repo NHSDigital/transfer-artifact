f63378016cb1c2e38f5db8b4cf71bc355de5a2a0
Alice Bell <alice.bell@bjss.com>
2024-02-26 08:23:54 +0000

    CAAS-2009: added myList3 using bucket and prefix as objects
    CAAS-2009: added myList3 using bucket and prefix as objects 2
    
    CAAS-2009: added myList3 using bucket and prefix as objects 3
    
    CAAS-2009: rewrote downloader to use new myList3 logic
    
    CAAS-2009: rewrote downloader to use new myList3 logic 2
    
    CAAS-2009: tidy
    
    CAAS-2009: tidy

--- src/aws/downloader.ts
+++ src/aws/downloader.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import fs from 'node:fs/promises';
 import {getInputs} from '../input-helper';
 import {listAllS3Objects } from "../aws";
-import { writeS3ObjectToFile } from './get-object-s3';
+import { listS3Objects, writeS3ObjectToFile } from './get-object-s3';
 import path from 'node:path';
 
 // used for getting the name of the item, which is the last part of the file path
@@ -25,55 +25,80 @@ export async function runDownload(): Promise<void> {
         console.log(`I am pipeline_id: ${pipeline_id}`)
         console.log(`I am key: ${path.join(bucket,'ci-pipeline-upload-artifacts',name)}`)
 
-        const myList2 = await listAllS3Objects(
+        console.log('Calling myList3:')
+        const myList3 = await listS3Objects(
           {
-            Bucket: bucket,
-            Key: path.join(bucket,'ci-pipeline-upload-artifacts',name),
-          },
-          startAfter
+            Bucket: 'caas-pl-490772702699-eu-west-2-pl-mdev-acct-cicd-temp-artifacts',
+            Prefix: name
+          }
         )
+
+        for(const item of myList3){
+          // without the if statement, it brings back everything
+          // see example here: https://github.com/NHSDigital/caas/actions/runs/8046725250/job/21974594476
+          if(item.includes(pipeline_id) || item.includes(`/pipeline_files/`)){
+            const newFilename = getItemName(item)
+              fs.writeFile(newFilename,'')
+
+              await writeS3ObjectToFile(
+                    {
+                      Bucket: bucket,
+                      Key: `${item}`
+                    },
+                    newFilename
+              )
+              console.log(`${item} has been downloaded to ${newFilename}`)
+                  }
+        }
+
+        
+        // console.log('Calling myList2:')
+        // const myList2 = await listAllS3Objects(
+        //   {
+        //     Bucket: bucket,
+        //     Key: path.join(bucket,'ci-pipeline-upload-artifacts',name),
+        //   },
+        //   startAfter
+        // )
 
         // listAllS3Objects brings back ALL objects
         // but we only want the ones for THIS Github pipeline
 
-        for(const item of myList2){
+        // for(const item of myList2){
 
-          if(item.includes(pipeline_id)){
-            console.log(`I am item: ${item}`)
-            // create and activate the new file before writing to it
-            const newFilename = getItemName(item)
-            fs.writeFile(newFilename,'')
+        //   if(item.includes(pipeline_id)){
+        //     console.log(`I am item: ${item}`)
+        //     // create and activate the new file before writing to it
+        //     const newFilename = getItemName(item)
+        //     fs.writeFile(newFilename,'')
 
-            await writeS3ObjectToFile(
-                  {
-                    Bucket: bucket,
-                    Key: `${item}`
-                  },
-                  newFilename
-            )
-            console.log(`${item} has been downloaded to ${newFilename}`)
-          }
+        //     await writeS3ObjectToFile(
+        //           {
+        //             Bucket: bucket,
+        //             Key: `${item}`
+        //           },
+        //           newFilename
+        //     )
+        //     console.log(`${item} has been downloaded to ${newFilename}`)
+        //   }
 
-          if(item.includes(`/pipeline_files/`)){
-            console.log(`I am json file: ${item}`)
-            // create and activate the new file before writing to it
-            const newFilename = path.join(getItemName(item))
-            console.log(`I am getItemNameWithSplit: ${newFilename}`)
-            fs.writeFile(newFilename,'')
+        //   if(item.includes(`/pipeline_files/`)){
+        //     console.log(`I am json file: ${item}`)
+        //     // create and activate the new file before writing to it
+        //     const newFilename = path.join(getItemName(item))
+        //     fs.writeFile(newFilename,'')
 
-            await writeS3ObjectToFile(
-                  {
-                    Bucket: bucket,
-                    // 2009 - this feels dodgy
-                    // tried adding pipeline_files/ to see if that deals with the problem of not finding files
-                    Key: item
-                  },
-                  newFilename
-            )
-            console.log(`JSON file ${item} has been downloaded to ${newFilename}`)
-          }
+        //     await writeS3ObjectToFile(
+        //           {
+        //             Bucket: bucket,
+        //             Key: item
+        //           },
+        //           newFilename
+        //     )
+        //     console.log(`JSON file ${item} has been downloaded to ${newFilename}`)
+          // }
 
-        }
+        // }
         console.log(`Items successfully downloaded: ${await fs.readdir(`./`)}`)
     } catch (error) {
         core.setFailed((error as Error).message)
