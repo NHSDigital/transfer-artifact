b03e52e8f6db09e2fb53292d4c00e5c3b8f038e8
Alice Bell <alice.bell@bjss.com>
2024-03-26 17:14:54 +0000

    CAAS-2168: tried a different method for getting the name set

--- dist/index.js
+++ dist/index.js
@@ -56369,7 +56369,8 @@ function raiseError(errorMessage) {
  * Helper to get all the inputs for the action
  */
 function getInputs() {
-    const name = core.getInput(Inputs.Name);
+    // const name = core.getInput(Inputs.Name)
+    const name = (core.getInput(Inputs.FolderName), "-", core.getInput(Inputs.FolderName)).concat();
     console.log(`I am name: ${name}`);
     const path = core.getInput(Inputs.Path, { required: true });
     const bucket = core.getInput(Inputs.ArtifactBucket) ||
