95fee69c9c18edd7f85215e441f9094c4e6f8f69
Alice Bell <alice.bell@bjss.com>
2024-04-10 16:33:19 +0100

    CAAS-2009: undid changes to prefix, no change

--- src/aws/downloader.ts
+++ src/aws/downloader.ts
@@ -46,8 +46,8 @@ export async function runDownload(): Promise<any> {
 
     const objectList = await listS3Objects({
       Bucket: bucket,
-      // Prefix: name,
-      Prefix: `ci-pipeline-upload-artifacts/${name}`
+      Prefix: name,
+      // Prefix: `ci-pipeline-upload-artifacts/${name}`
     });
 
     console.log(`I am objectList: ${objectList}`)
