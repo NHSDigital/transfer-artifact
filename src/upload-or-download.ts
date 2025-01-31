0b103a4fc44afc7a9b2010244701693e97c33e91
Fran <francesco.galassi1@nhs.net>
2025-01-31 11:14:20 +0000

    CAAS-3169: PR
    CAAS-3169: PR - prettier

--- src/upload-or-download.ts
+++ src/upload-or-download.ts
@@ -6,10 +6,14 @@ const direction = getInputs().direction;
 
 if (direction === 'upload') {
   console.log('Starting upload...');
-  runUpload().then(() => console.log('Upload completed!'));
+  runUpload()
+    .then(() => console.log('Upload completed!'))
+    .catch((error) => console.log('An error occurred: ', error));
 } else if (direction === 'download') {
   console.log('Starting download...');
-  runDownload().then(() => console.log('Download completed!'));
+  runDownload()
+    .then(() => console.log('Download completed!'))
+    .catch((error) => console.log('An error occurred: ', error));
 } else {
   console.log(
     'No input found for direction.  Please specify "upload" or "download".'
